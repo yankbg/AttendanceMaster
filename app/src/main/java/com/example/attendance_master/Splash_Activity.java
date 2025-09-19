6262695fbc098dd573d6cf6c3428d92f6b98167f
YANNICK kabunga <yankbg2@gmail.com>
2025-09-19 17:44:05 +0300

    we implemented our Splash_Activity by putting some UI design and the java class

--- app/src/main/java/com/example/attendance_master/Splash_Activity.java
+++ app/src/main/java/com/example/attendance_master/Splash_Activity.java
@@ -2,7 +2,10 @@ package com.example.attendance_master;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class Splash_Activity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Splash_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                    Intent intent = new Intent(Splash_Activity.this, MainActivity.class);
+                    startActivity(intent);
+            }
+        }, 1500);
     }
 }
